23d0bc1e1930f6f08934583dee09ffbcfeb4cc4a
Fransis96 <fransiskus.agustinus@students.utdi.ac.id>
2025-03-30 10:07:13 +0700

    Update file bat

--- tesRunTeks.bat
+++ tesRunTeks.bat
@@ -1,6 +1,4 @@
 @echo off
-REM Mengompilasi file Teks.java dan Colors.java ke direktori bin
-javac -d bin .\tes123\Teks.java .\tes123\Colors.java
 
 REM Mengompilasi file Teks.java, Colors.java, dan Utama.java ke direktori bin
 javac -d bin .\tes123\Teks.java .\tes123\Colors.java .\tes123\Utama.java
