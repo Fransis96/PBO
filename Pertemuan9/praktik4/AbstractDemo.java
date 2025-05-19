cf87504685ad55b44fd4829c48bf1464e6a17572
Fransis96 <fransiskus.agustinus@students.utdi.ac.id>
2025-05-19 21:03:44 +0700

    Update repo

---
+++ Pertemuan9/praktik4/AbstractDemo.java
@@ -0,0 +1,13 @@
+public class AbstractDemo {
+    public static void main(String[] args) {
+
+        Gaji s = new Gaji("Mohd Mohtashim", "Ambehta, Up", 3, 3600.00);
+        Karyawan e = new Gaji("John Adams", "Boston, MA", 2, 2400.00);
+
+        System.out.println("Panggil mailCheck pakai Gaji");
+        s.mailCheck();
+
+        System.out.println("\n Panggil mailCheck pakai Karyawan");
+        e.mailCheck();
+    }
+}
